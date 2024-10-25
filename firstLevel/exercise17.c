9b41a0e7c962b58a5b7e36b49099d80ba2bf3d49
Andrés Felipe P <andresfelipepiedrahitamorales@gmail.com>
2024-10-25 04:09:28 -0500

    update exercise17

--- firstLevel/exercise17.c
+++ firstLevel/exercise17.c
@@ -1,1 +1,28 @@
-//17.
+//17. Leer un número entero de tres dígitos y determinar en qué posición está el mayor dígito.
+
+#include <stdio.h>
+
+int main() {
+    int number, digit, maxDigit = 0, maxPosition = 0;
+    printf("Enter a three-digit number: ");
+    scanf("%d", &number);
+    digit = number % 10;
+    if (digit > maxDigit) {
+        maxDigit = digit;
+        maxPosition = 3;
+    }
+    number /= 10;
+    digit = number % 10;
+    if (digit > maxDigit) {
+        maxDigit = digit;
+        maxPosition = 2;
+    }
+    number /= 10;
+    digit = number % 10;
+    if (digit > maxDigit) {
+        maxDigit = digit;
+        maxPosition = 1;
+    }
+    printf("The largest digit is %d and is in position %d.\n", maxDigit, maxPosition);
+    return 0;
+}
